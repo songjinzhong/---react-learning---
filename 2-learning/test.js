c9df947a46a87a4f305b8843e500330e9510d1aa
songjinzhong <648660165@qq.com>
2016-09-24 22:39:49 +0800

    delete unuse file

--- 2-learning/test.js
+++
@@ -1,9 +0,0 @@
-var sleep = require('co-sleep');
-var co = require('co');
- 
-co(function *() {
-  var now = Date.now();
-  // wait for 1000 ms 
-  yield sleep(1000);
-  expect(Date.now() - now).to.not.be.below(1000);
-})();
