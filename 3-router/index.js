50a2461bb12818484ff6e7b594abb64b95f62dfd
songjinzhong <648660165@qq.com>
2016-12-13 23:47:18 +0800

    add Repo status

--- 3-router/index.js
+++ 3-router/index.js
@@ -3,12 +3,14 @@ import { render } from 'react-dom'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
+import Repo from './modules/Repo'
 import { Router, Route, hashHistory } from 'react-router'
 render(
 	(<Router history={hashHistory}>
 			<Route path="/" component={App}>
 			 <Route path="/about" component={About}/>
-			 <Route path="/repos" component={Repos}/>
+       <Route path="/repos" component={Repos}/>
+			 <Route path="/repos/:userName/:repoName" component={Repo}/>
       </Route>
 		</Router>)
 	, document.getElementById('app'))
