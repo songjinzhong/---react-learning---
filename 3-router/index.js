842bb487e537a9034d082a21a9e02ab94c635c61
songjinzhong <648660165@qq.com>
2016-12-14 20:16:09 +0800

    add Nesting

--- 3-router/index.js
+++ 3-router/index.js
@@ -8,9 +8,10 @@ import { Router, Route, hashHistory } from 'react-router'
 render(
 	(<Router history={hashHistory}>
 			<Route path="/" component={App}>
-			 <Route path="/about" component={About}/>
-       <Route path="/repos" component={Repos}/>
-			 <Route path="/repos/:userName/:repoName" component={Repo}/>
+			  <Route path="/about" component={About}/>
+        <Route path="/repos" component={Repos}>
+			    <Route path="/repos/:userName/:repoName" component={Repo}/>
+        </Route>
       </Route>
 		</Router>)
 	, document.getElementById('app'))
