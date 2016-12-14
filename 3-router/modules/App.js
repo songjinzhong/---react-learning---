133ee6370e668c89d147e11b51f31f2209b8c17f
songjinzhong <648660165@qq.com>
2016-12-14 21:14:10 +0800

    add 首页索引

--- 3-router/modules/App.js
+++ 3-router/modules/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link} from 'react-router'
 import NavLink from './NavLink'
 import Home from './Home'
+import { IndexLink } from 'react-router'
 
 export default React.createClass({
   render() {
@@ -9,11 +10,12 @@ export default React.createClass({
     	<div>
         <h1>React Router Tutorial</h1>
         <ul role="nav">
+          {/* <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li> */}
+          <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/repos">Repos</NavLink></li>
         </ul>
         {this.props.children }
-        //{this.props.children || <Home/>}
       </div>
     )
   }
