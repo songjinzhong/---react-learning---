69010e48c428115ecc4c1d6513e6c1c649915192
songjinzhong <648660165@qq.com>
2016-12-15 20:18:19 +0800

    add 路由系统

--- 3-router/modules/Repos.js
+++ 3-router/modules/Repos.js
@@ -1,9 +1,22 @@
 // modules/Repos.js
 import React from 'react'
-import {Link} from 'react-router'
 import NavLink from './NavLink'
 
 export default React.createClass({
+
+  contextTypes:{
+    router: React.PropTypes.object
+  },
+
+  handSubmit(event){
+    event.preventDefault();
+    const userName = event.target.elements[0].value
+    const repo = event.target.elements[1].value
+    const path = `/repos/${userName}/${repo}`
+    console.log(path)
+    this.context.router.push(path)
+  },
+
   render() {
     return (
       <div>
@@ -11,7 +24,15 @@ export default React.createClass({
         <ul>
           <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
           <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+          <li>
+            <form onSubmit={this.handSubmit}>
+              <input type="text" placeholder="userName"/> / {' '}
+              <input type="text" placeholder="repo"/>{' '}
+              <button type="submit">Go</button>
+            </form>
+          </li>
         </ul>
+
         {this.props.children}
       </div>
     )
