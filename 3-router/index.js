1c4f9556508ac0b67da78d2f6b28d33d15a0b9e8
songjinzhong <648660165@qq.com>
2016-12-15 20:51:45 +0800

    添加 express 路由设置

--- 3-router/index.js
+++ 3-router/index.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './modules/App'
-import About from './modules/About'
-import Repos from './modules/Repos'
-import Repo from './modules/Repo'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
-import Home from './modules/Home'
+import { Router, browserHistory } from 'react-router'
+// import routes and pass them into <Router/>
+import routes from './modules/routes'
+
 render(
-	(<Router history={browserHistory}>
-			<Route path="/" component={App}>
-        <IndexRoute component={Home}/>
-			  <Route path="/about" component={About}/>
-        <Route path="/repos" component={Repos}>
-			    <Route path="/repos/:userName/:repoName" component={Repo}/>
-        </Route>
-      </Route>
-		</Router>)
-	, document.getElementById('app'))
+  <Router routes={routes} history={browserHistory}/>,
+  document.getElementById('app')
+)
