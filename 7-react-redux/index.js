3dc9b836f42fabf9d5a6d57690b3196516432266
songjinzhong <648660165@qq.com>
2016-12-22 22:42:53 +0800

    add react-redux demo

---
+++ 7-react-redux/index.js
@@ -0,0 +1,7 @@
+import React, { Component, PropTypes } from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <p>Hello</p>,
+  document.getElementById('example')
+)
---
+++ 7-react-redux/webpack.config.js
@@ -0,0 +1,13 @@
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders:[
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+    ]
+  }
+}
